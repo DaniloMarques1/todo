4c246bb906189a8d8c4bfc4a04671930f45712ef
Danilo Marques <danilomarques20@hotmail.com>
2019-08-23 15:00:19 -0300

    Updates

---
+++ precache-manifest.021169ee949208fe088ed7d1506350bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab07cc499335fdf8b7f52987a25cc2db",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "9c27bc18460bd76f23f4",
+    "url": "/todo/static/css/main.655ec421.chunk.css"
+  },
+  {
+    "revision": "a08f1c3a09428297d921",
+    "url": "/todo/static/js/2.941bb39d.chunk.js"
+  },
+  {
+    "revision": "9c27bc18460bd76f23f4",
+    "url": "/todo/static/js/main.0bfd2b80.chunk.js"
+  },
+  {
+    "revision": "bda5c4bedbd2155e3160",
+    "url": "/todo/static/js/runtime~main.c4f80c79.js"
+  },
+  {
+    "revision": "39b6b69ab582b4d8fd45685981586323",
+    "url": "/todo/static/media/delete.39b6b69a.svg"
+  }
+]);
